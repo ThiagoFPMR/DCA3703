2c9f01c4db64dc70772fa7149ef0f0319e4a2c3b
ThiagoFPMR <thiago.rodrigues.056@ufrn.edu.br>
2025-04-22 09:30:26 -0300

    task 7

---
+++ task7/fake_files_parallel.c
@@ -0,0 +1,81 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "omp.h"
+
+struct node 
+{
+    char *filename;
+    struct node *next;
+};
+
+// Fill linked list with fake incremental file names
+void fill_linked_list(struct node *head, int n)
+{
+    struct node *current = head;
+    for (int i = 0; i < n; i++)
+    {
+        current->filename = malloc(20);
+        sprintf(current->filename, "file%d.txt", i);
+        if (i < n - 1) 
+        { // Only allocate a new node if it's not the last one
+            current->next = malloc(sizeof(struct node));
+            current = current->next;
+        } 
+        else 
+            current->next = NULL; // Set the last node's next pointer to NULL
+    }
+    current->next = NULL;
+}
+
+// Free linked list
+void free_linked_list(struct node *head)
+{
+    struct node *current = head;
+    struct node *next_node;
+    while (current != NULL)
+    {
+        next_node = current->next;
+        free(current->filename);
+        free(current);
+        current = next_node;
+    }
+}
+
+void print_linked_list(struct node *head)
+{
+    struct node *current = head;
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            // Print the thread creating the tasks
+            printf("Criando tarefas na thread %d\n", omp_get_thread_num());
+            while (current != NULL)
+            {
+                struct node *node_copy = current;
+                #pragma omp task firstprivate(node_copy)
+                {
+                    printf("Processando %s na thread %d\n", node_copy->filename, omp_get_thread_num());
+                }
+                current = current->next;
+            }
+            // Wait for all tasks to finish
+            #pragma omp taskwait
+        }
+    }
+}
+int main()
+{
+    int n = 100; // Number of files
+    printf("n = %d\n", n);
+    // Allocate memory for the linked list
+    struct node *head = malloc(sizeof(struct node));
+    fill_linked_list(head, n);
+    // Print the file names
+    print_linked_list(head);
+    // Free the linked list
+    free_linked_list(head);    
+
+    return 0;
+}
